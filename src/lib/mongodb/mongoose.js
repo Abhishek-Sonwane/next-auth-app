42e6533b1e8d3b2016f05a61b19d25dce44feea8
Abhishek Sonwane <abhisheksonwane629@gmail.com>
2025-03-26 17:23:16 +0530

    .

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -12,7 +12,7 @@ export const connect = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "next auth app",
+      dbName: "next_auth_app",
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
     });
